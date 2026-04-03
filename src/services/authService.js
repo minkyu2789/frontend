65e4ff53da8ac98e3b08871b6d28057f2b79b4b7
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 21:47:35 +0900

    Include profile image URL in signup payload

--- src/services/authService.js
+++ src/services/authService.js
@@ -19,6 +19,7 @@ export async function signup({
   phone,
   sex,
   introduction,
+  profileImageUrl,
   nationalityId,
   keywordIds = [],
 }) {
@@ -30,6 +31,7 @@ export async function signup({
     phone,
     sex,
     introduction,
+    profileImageUrl,
     nationalityId,
     keywordIds,
   });
